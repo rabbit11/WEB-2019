d215540ff01488d1ee2ad3a6d7180f945da81a0c
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-07 20:34:21 -0300

    Only missing icon on navbar and something in the jumbotron

--- assignment/src/components/Home/index.js
+++ assignment/src/components/Home/index.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Nav, Jumbotron, Container, Row, Col } from "react-bootstrap";
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import posed from 'react-pose';
 import "./styles.css";
 
 const StyledJumbotron = styled(Jumbotron)`
-  background-color: transparent;
+  background: transparent;
+  margin-top: 200px;
 `;
 
-const StyledButton = styled(Button)`
-background-image: linear-gradient(180deg, #2af598 0%, #009efd 100%);
-`;
+const element = <FontAwesomeIcon icon={faCoffee} />
+
 
 const Home = () => (
   <div className="app-wrapper">
     <div className="header">
-      <Navbar defaultActiveKey="/home" fixed="top">
+      <Navbar defaultActiveKey="/home" fixed="top" id="mainNavbar">
         <Container>
-          <Navbar.Brand href="/home">IS</Navbar.Brand>
+          <Navbar.Brand href="/home"><FontAwesomeIcon icon="coffee"/>IS</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Nav className="justify-content-end">
-                <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="/home" className="active">Home</Nav.Link>
+              <Nav.Link href="#link">Lista</Nav.Link>
             </Nav>
         </Container>
       </Navbar>
@@ -33,26 +36,26 @@ const Home = () => (
             Um simples site onde você pode encontrar todas as informações necessárias sobre um estrangeiro no país.
           </p>
           <p>
-          <Button href="#" variant="outline-primary my-4" size="lg">Prosseguir</Button>
+          <Button href="#" variant="outline-light my-4" size="lg">Prosseguir</Button>
           </p>
-        <img src="" className="projectImage"></img>
+          <img src="" className="projectImage"></img>
           {/* <img variant="mt-5 mb-0" src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img> */}
         </Container>
       </StyledJumbotron>
     <div id="aboutContent">
       <Container>
         <Row>
-          <Col md={6}><img className="projectImage" src="https://png2.kisspng.com/sh/9d391a12c12529cc6b03e6b1bac5b725/L0KzQYm3UsI5N5ZofZH0aYP2gLBuTfJ2e5pzfeV8LXfycb20ggBxdJYyjtdsdHB1PbP8kBlvbaR4ReJub4DvdX7wkB9ubaV3gdU2NXG8Roq8gck3PZZofqc3M0i4R4m6WMgyPWIAUKMDMUW2RIKAVb5xdpg=/kisspng-business-goal-apple-vector-business-people-isometric-5a9695a965ecf5.3857838815198181534175.png"></img></Col>
+          <Col md={6}><img className="projectImage" src="https://png2.kisspng.com/sh/d34c2f4ed6a6b607d2e879c218c79811/L0KzQYm3WMEzN6t8e5H0aYP2gLBuTfh2dZJzReRuc3B4grTsTf1idpJsfd9uboSwdMPwgvJjdJYygdDtdYP3gsq0gB9ueJJzRdp4bXWwgLFAhgJib5ZzjJ8AYkbpdoSBVMUybGc4TZC6NkO2R4SCVME2O2U1TqU9OUK2Q4a5TwBvbz==/kisspng-human-resource-management-dribbble-industry-compan-home-poweragent-5b6ff38451d635.1633739415340634923352.png"></img></Col>
           <Col md={6} className="align-self-center">
-            <p class="contentTitle">Sobre Immigrant Song</p>
+            <p className="contentTitle">Sobre Immigrant Song</p>
             <p>Aqui você pode encontrar diversas informações sobre um estrangeiro no país,
               como data de entrada e saída, número de seu passaporte, nacionalidade, entre outras.
             </p>
           </Col>
         </Row>
-        <Row>
+        <Row className="mt-0">
           <Col md={6} className="align-self-center">
-            <p class="contentTitle">Sobre este Projeto</p>
+            <p className="contentTitle">Sobre este Projeto</p>
             <p>Esta plataforma foi desenvolvida como um trabalho integrado, para as disciplinas
               de Engenharia de Software 2 e Desenvolvimento Web. Durante o desenvolvimento deste trabalho, utilizamos todo nosso conhecimento adquirido
             durante o semestre, deixando nosso lado criativo fluir para a construção de um site simples e 
@@ -64,7 +67,7 @@ const Home = () => (
         <Row>
           <Col md={6}><img src="http://hackncs.com/img/team.png" className="projectImage"></img></Col>
           <Col md={6} className="align-self-center">
-          <p class="contentTitle">Nossa Equipe</p>
+          <p className="contentTitle">Nossa Equipe</p>
           <p>Todos os integrantes do grupo fizeram parte de cada uma das atividades do projeto,
             trabalhando ao máximo para que esse projeto fosse algo criado por nós, deixando a marca
             de cada um e construindo algo que pudéssemos nos orgulhar.
