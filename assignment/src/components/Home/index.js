6e2a683ece38b8ea3bddda56aa19144fc67409e8
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-07 17:50:49 -0300

    Homepage almost done

--- assignment/src/components/Home/index.js
+++ assignment/src/components/Home/index.js
@@ -4,9 +4,13 @@ import { Button, Navbar, Nav, Jumbotron, Container, Row, Col } from "react-boots
 import styled from 'styled-components'
 import "./styles.css";
 
-// const Jumbotron = styled.Jumbotron`
-//   border: 1px solid red
-// `
+const StyledJumbotron = styled(Jumbotron)`
+  background-color: transparent;
+`;
+
+const StyledButton = styled(Button)`
+background-image: linear-gradient(180deg, #2af598 0%, #009efd 100%);
+`;
 
 const Home = () => (
   <div className="app-wrapper">
@@ -22,8 +26,8 @@ const Home = () => (
         </Container>
       </Navbar>
     </div>
-      <Jumbotron className="styled-jumbotron">
-        <Container className="mt-5 text-center">
+      <StyledJumbotron>
+        <Container className="mt-5 text-center justify-content-center">
           <h1>Bem vindo ao Immigrant Song</h1>
           <p className="mt-3">
             Um simples site onde você pode encontrar todas as informações necessárias sobre um estrangeiro no país.
@@ -31,30 +35,40 @@ const Home = () => (
           <p>
           <Button href="#" variant="outline-primary my-4" size="lg">Prosseguir</Button>
           </p>
-          <img variant="mt-5 mb-0" src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img>
+        <img src="" className="projectImage"></img>
+          {/* <img variant="mt-5 mb-0" src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img> */}
         </Container>
-      </Jumbotron>
-    <div>
+      </StyledJumbotron>
+    <div id="aboutContent">
       <Container>
         <Row>
-          <Col md={6}><img src="http://hackncs.com/img/project.png" id="projectImage"></img></Col>
+          <Col md={6}><img className="projectImage" src="https://png2.kisspng.com/sh/9d391a12c12529cc6b03e6b1bac5b725/L0KzQYm3UsI5N5ZofZH0aYP2gLBuTfJ2e5pzfeV8LXfycb20ggBxdJYyjtdsdHB1PbP8kBlvbaR4ReJub4DvdX7wkB9ubaV3gdU2NXG8Roq8gck3PZZofqc3M0i4R4m6WMgyPWIAUKMDMUW2RIKAVb5xdpg=/kisspng-business-goal-apple-vector-business-people-isometric-5a9695a965ecf5.3857838815198181534175.png"></img></Col>
           <Col md={6} className="align-self-center">
-            <p class="contentTitle">Sobre este Projeto</p>
-            <p>Essa plataforma foi desenvolvida como um trabalho integrado, para as disciplinas
-              de Engenharia de Software 2 e Desenvolvimento Web. Durante o desenvolvimento deste trabalho, utilizamos todo nosso conhecimento adquirido
-            durante o semestre, deixando nosso lado criativo fluir para a construção de um site simples e 
-            atrativo.
+            <p class="contentTitle">Sobre Immigrant Song</p>
+            <p>Aqui você pode encontrar diversas informações sobre um estrangeiro no país,
+              como data de entrada e saída, número de seu passaporte, nacionalidade, entre outras.
             </p>
           </Col>
         </Row>
         <Row>
           <Col md={6} className="align-self-center">
+            <p class="contentTitle">Sobre este Projeto</p>
+            <p>Esta plataforma foi desenvolvida como um trabalho integrado, para as disciplinas
+              de Engenharia de Software 2 e Desenvolvimento Web. Durante o desenvolvimento deste trabalho, utilizamos todo nosso conhecimento adquirido
+            durante o semestre, deixando nosso lado criativo fluir para a construção de um site simples e 
+            atrativo.
+            </p>
+          </Col>
+          <Col md={6}><img src="http://hackncs.com/img/project.png" className="projectImage"></img></Col>
+        </Row>
+        <Row>
+          <Col md={6}><img src="http://hackncs.com/img/team.png" className="projectImage"></img></Col>
+          <Col md={6} className="align-self-center">
           <p class="contentTitle">Nossa Equipe</p>
           <p>Todos os integrantes do grupo fizeram parte de cada uma das atividades do projeto,
             trabalhando ao máximo para que esse projeto fosse algo criado por nós, deixando a marca
-            de cada um, do seu jeito e construir algo que pudéssemos nos orgulhar.
+            de cada um e construindo algo que pudéssemos nos orgulhar.
           </p></Col>
-          <Col md={6}><img src="http://hackncs.com/img/team.png"></img></Col>
         </Row>
       </Container>
     </div>
