ea662ac529589d3ab4c732ebc20803effbdf8328
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-07 11:27:43 -0300

    homepage basics done

--- assignment/src/components/Home/index.js
+++ assignment/src/components/Home/index.js
@@ -1,43 +1,43 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Navbar, Nav, Jumbotron } from "react-bootstrap";
+import { Button, Navbar, Nav, Jumbotron, Container, Row, Col } from "react-bootstrap";
 import styled from 'styled-components'
 import "./styles.css";
-
-// const StyledJumbotron = styled.Jumbotron`
-//     background: #000046;  /* fallback for old browsers */
-//     background: -webkit-linear-gradient(to right, #1CB5E0, #000046);  /* Chrome 10-25, Safari 5.1-6 */
-//     background: linear-gradient(to right, #1CB5E0, #000046); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-//     color: red;
-// `
 
 const Home = () => (
   <div className="app-wrapper">
     <div className="header">
-        <Navbar bg="light" expand="lg">
-      <div className="container">
-            <Navbar.Brand href="#home">IS</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Nav className="justify-content-end">
+      <Navbar defaultActiveKey="/home" bg="light" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="/home">IS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="justify-content-end">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-              </Nav>
-      </div>
-          </Navbar>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
     </div>
-      <Jumbotron className={styled.jumbotron}>
-        <div className="container mt-5 text-center">
+      <Jumbotron className="styled-jumbotron">
+        <Container className="mt-5 text-center">
           <h1>Bem vindo ao Immigrant Song</h1>
-          <p>
+          <p className="mt-3">
             Um simples site onde você pode encontrar todas as informações necessárias sobre um estrangeiro no país.
           </p>
-          <img className="mt-5 mb-0"src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img>
           <p>
-            <Button variant="primary mt-5">Prosseguir</Button>
+          <Button href="#" variant="outline-primary my-4" size="lg">Prosseguir</Button>
           </p>
-        </div>
+          <img variant="mt-5 mb-0" src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img>
+        </Container>
       </Jumbotron>
-  
+    <div>
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+    </div>
   </div>
 );
 
