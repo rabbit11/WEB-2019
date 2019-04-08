02d52579089d172addac60916003a308208282cd
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-08 18:27:26 -0300

    Homepage is done? Missing some animations

--- assignment/src/components/Home/index.js
+++ assignment/src/components/Home/index.js
@@ -17,6 +17,7 @@ const element = <FontAwesomeIcon icon={faCoffee} />
 
 const Home = () => (
   <div className="app-wrapper">
+  {/* -------------- Navbar ------------------------ */}
     <div className="header">
       <Navbar defaultActiveKey="/home" fixed="top" id="mainNavbar">
         <Container>
@@ -29,6 +30,8 @@ const Home = () => (
         </Container>
       </Navbar>
     </div>
+  {/* -------------- Navbar ------------------------ */}
+  {/* ------------------ Introdução -------------------- */}
       <StyledJumbotron>
         <Container className="mt-5 text-center justify-content-center">
           <h1>Bem vindo ao Immigrant Song</h1>
@@ -42,10 +45,12 @@ const Home = () => (
           {/* <img variant="mt-5 mb-0" src="http://www.pngmart.com/files/4/Travel-PNG-Image.png"></img> */}
         </Container>
       </StyledJumbotron>
+    {/* ------------------ Introdução -------------------- */}
+    {/* ------------------ Sobre -------------------- */}
     <div id="aboutContent">
       <Container>
         <Row>
-          <Col md={6}><img className="projectImage" src="https://png2.kisspng.com/sh/d34c2f4ed6a6b607d2e879c218c79811/L0KzQYm3WMEzN6t8e5H0aYP2gLBuTfh2dZJzReRuc3B4grTsTf1idpJsfd9uboSwdMPwgvJjdJYygdDtdYP3gsq0gB9ueJJzRdp4bXWwgLFAhgJib5ZzjJ8AYkbpdoSBVMUybGc4TZC6NkO2R4SCVME2O2U1TqU9OUK2Q4a5TwBvbz==/kisspng-human-resource-management-dribbble-industry-compan-home-poweragent-5b6ff38451d635.1633739415340634923352.png"></img></Col>
+          <Col md={6}><img src="https://png2.kisspng.com/sh/30d3cc4fb4dafe5de8481a30c459fe34/L0KzQYm3UsE5N5x8iZH0aYP2gLBuTflvbpDsitN5aHnmPbr6jB1mfKNue595cnBtdbT7if9vNZRteeR9LYboc8X2kr11aZNxfeY2NXG7SYi3U8c2a2I1Tqk3N0m1RoK9WcgyPWI9UacBNUm8Q4iAUb5xdpg=/kisspng-infographic-isometric-projection-chart-vector-tablet-5a8970375c1067.7926169815189565993771.png" className="projectImage"></img></Col>
           <Col md={6} className="align-self-center">
             <p className="contentTitle">Sobre Immigrant Song</p>
             <p>Aqui você pode encontrar diversas informações sobre um estrangeiro no país,
@@ -62,10 +67,12 @@ const Home = () => (
             atrativo.
             </p>
           </Col>
-          <Col md={6}><img src="http://hackncs.com/img/project.png" className="projectImage"></img></Col>
+          {/* <Col md={6}><img src="http://hackncs.com/img/project.png" className="projectImage"></img></Col> */}
+          <Col md={6}><img src="https://png2.kisspng.com/sh/e149439ca9b26b8bffa5ad148c61d692/L0KzQYm3VcExN6h9fZH0aYP2gLBuTgN1d5RwReJxb4Tyd8PokPh6NZR0heJAdHX1PbT2kwBwepJ5fZ9yZHXxhLr7mb1mdJZyfdD9LYP3ccXwjB5meqoyTdNvM3TncrPoUcQ5PGQzUKs5OUa1QIK4VcI2QWQ2TKc6NkazRnB3jvc=/kisspng-stock-photography-computer-corporate-identity-element-stationery-5af3ddbba14843.8909620115259314516606.png" className="projectImage"></img></Col>
         </Row>
         <Row>
-          <Col md={6}><img src="http://hackncs.com/img/team.png" className="projectImage"></img></Col>
+          {/* <Col md={6}><img src="http://hackncs.com/img/team.png" className="projectImage"></img></Col> */}
+          <Col md={6}><img className="projectImage" src="https://png2.kisspng.com/sh/d34c2f4ed6a6b607d2e879c218c79811/L0KzQYm3WMEzN6t8e5H0aYP2gLBuTfh2dZJzReRuc3B4grTsTf1idpJsfd9uboSwdMPwgvJjdJYygdDtdYP3gsq0gB9ueJJzRdp4bXWwgLFAhgJib5ZzjJ8AYkbpdoSBVMUybGc4TZC6NkO2R4SCVME2O2U1TqU9OUK2Q4a5TwBvbz==/kisspng-human-resource-management-dribbble-industry-compan-home-poweragent-5b6ff38451d635.1633739415340634923352.png"></img></Col>
           <Col md={6} className="align-self-center">
           <p className="contentTitle">Nossa Equipe</p>
           <p>Todos os integrantes do grupo fizeram parte de cada uma das atividades do projeto,
@@ -75,6 +82,7 @@ const Home = () => (
         </Row>
       </Container>
     </div>
+    {/* ------------------ Sobre -------------------- */}
   </div>
 );
 
